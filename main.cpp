21c0d37b335fed34e1c9d56cc89b2a13860f89ef
sneevilman <sneevilman@gmail.com>
2017-08-09 15:49:21 -0700

    okay now it actually works

--- main.cpp
+++ main.cpp
@@ -7,8 +7,10 @@
 
 //System Libraries Here
 #include <iostream>
+#include <cstdlib>
 #include <fstream>
 #include <string>
+#include <vector>
 using namespace std;
 
 //User Libraries Here
@@ -16,57 +18,57 @@ using namespace std;
 
 //Function Prototypes Here
 bool Restart();
+
 //Program Execution Begins Here
 
 int main() {
     //Declare and process variables
+    short size=1000;
     bool restart;
-    long q(64000);
-    string word[q],
-            place[q];
+    short cnt[size]={0};
+    long q;
+    string word[size];
+    string place[size];
     bool check=false;
+    bool chkd[size]={false};
     
     //Process/Calculations Here
     do {
-        q=0;
         ifstream in;
         in.open("zipf.txt");
-        while(in){
-            in>>word[q];
-            cout<<q<<": "<<word[q]<<endl;
-            if(word[q]==""){
-                cout<<"it worked"<<endl;
+        
+        q=0;
+        
+        for(short i=0;i<=q;i++){
+            in>>word[i];
+            cout<<word[i]<<endl;
+            if(word[i]==""){
                 in.close();
-            }
-            q++;
+            }else q++;
         }
+        cout<<"q: "<<q<<endl;
+        
         short n=0;
-        short cnt[q]={0};
+        for(short i=0;i<q;i++){
+            if(chkd[i]==false){
+                place[n]=word[i];
+                cnt[n]++;
+                chkd[i]=true;
+                for(short l=i+1;l<q;l++){
+                    if(word[i]==word[l]){
+                        cnt[i]++;
+                        chkd[l]=true;
+                    }
+                }
             
-            for(short i=0;i<q;i++){
-                check=true;
-                if(i>0){
-                    for(short l=0;l<i;l++){
-                        if(word[i]==word[l]){
-                            check=false;
-                        }
-                    }
-                }
-                if(check==true){
-                    for(short l=0;l<q;l++){
-                        if(word[i]==word[l]){
-                            cnt[n]++;
-                        }
-                    }
-                    place[n]=word[i];
-                    n++;
-                }
             }
+            
+            if(cnt[n]>0)n++;
+        }
+        cout<<"n: "<<n<<endl;
         for (short i=0;i<n;i++){
             cout<<place[i]<<": "<<cnt[i]<<endl;
         }
-        
-            in.close();
             
         restart = Restart();
     } while (restart);
